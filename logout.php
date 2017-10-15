82e5f0d3f60e116db60fe82bc3b3d3e7e9542d70
wudtichaikarun <595159090004@dpu.ac.th>
2017-10-15 23:35:46 +0700

    :astonished: check tab check indent

--- logout.php
+++ logout.php
@@ -5,19 +5,16 @@
     <title>logout</title>
   </head>
   <body>
-<h2>logout.php</h2>
-<?
-session_start();
-$UserId = $_SESSION["UserId"];
-
-include("conection.php");
-
-$sql = "insert into Book_read_log values(NULL, '$UserId', 'logout', NULL)";
-$query = mysqli_query($con, $sql);
-session_destroy();
-header("Location: iBookLogin.php");
-
-mysqli_close($con);
-?>
+    <h2>logout.php</h2>
+    <?php
+      session_start();
+      $UserId = $_SESSION["UserId"];
+      include("conection.php");
+      $sql = "insert into Book_read_log values(NULL, '$UserId', 'logout', NULL)";
+      $query = mysqli_query($con, $sql);
+      session_destroy();
+      header("Location: iBookLogin.php");
+      mysqli_close($con);
+    ?>
   </body>
 </html>
--- updateUserLevel.php
+++ updateUserLevel.php
@@ -1,23 +1,13 @@
 <?php
-    include("conection.php");
-
-    $userID = $_POST['user_id_update'];
-    $userLevel = $_POST['user_level_update'];
-    $act = $_POST['act'];
-
-    if ($act == 'update'){
-      $sql= "UPDATE ibookuser SET user_level='$userLevel' WHERE user_id='$userID'";
-      // execute the query.
-      $result = mysqli_query($con, $sql);
-    }else if($act == 'delete'){
-      $sql= "DELETE from ibookuser  WHERE user_id='$userID'";
-      // execute the query.
-      $result = mysqli_query($con, $sql);
-
-      // $sql2= "DELETE from favorite  WHERE user_id='$userID'";
-      // // execute the query.
-      // $result = mysqli_query($con, $sql2);
-    }
-
-
+  include("conection.php");
+  $userID = $_POST['user_id_update'];
+  $userLevel = $_POST['user_level_update'];
+  $act = $_POST['act'];
+  if ($act == 'update') {
+    $sql= "UPDATE ibookuser SET user_level='$userLevel' WHERE user_id='$userID'";
+    $result = mysqli_query($con, $sql);
+  } else if ($act == 'delete') {
+    $sql= "DELETE from ibookuser  WHERE user_id='$userID'";
+    $result = mysqli_query($con, $sql);
+  }
 ?>
